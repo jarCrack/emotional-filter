3070a852bff28fd2a4c4d7b80820744157310691
jmalich <malich@uni-potsdam.de>
2015-11-29 01:52:58 +0100

    Java Interface

--- analysis/detect_emotion.py
+++ analysis/detect_emotion.py
@@ -1,44 +1,50 @@
 import numpy as np
 import pandas
+import sys
 
 
-word="lake"
 
 
-#emotions={0.:"pleased",45.:"excited",90.:"aroused",135.:"distressed",180.:"miserable",225.:"depressed",270.:"sleepy",315.:"relaxed"}
+if(len(sys.argv)<2):
+	print "Please input tag"
+else:
+	#print sys.argv[1]
 
-emotions=["pleased","excited","aroused","distressed","miserable","depressed","sleepy","relaxed"]
+	word=sys.argv[1]
+	emotions=["pleased","excited","aroused","distressed","miserable","depressed","sleepy","relaxed"]
 
-"aroused,distressed,miserable,depressed,sleepy,relaxed,pleased,excited"
+	"aroused,distressed,miserable,depressed,sleepy,relaxed,pleased,excited"
 
-#scan csv
+	#scan csv
 
-df=pandas.read_csv("ratings.csv")
+	df=pandas.read_csv("ratings.csv")
 
-#print df
-entry=df[df["Word"]==word]
-valence=entry["V.Mean.Sum"]
-arousal=entry["A.Mean.Sum"]
+	#print df
+	entry=df[df["Word"]==word]
+	valence=entry["V.Mean.Sum"]
+	arousal=entry["A.Mean.Sum"]
 
-# compute degree
-arousal-=5.
-valence-=5.
+	# compute degree
+	arousal-=5.
+	valence-=5.
 
-deg=np.array(map(lambda x:360+x if x<0 else x,np.arctan2(arousal,valence)*360/(2*np.pi)))[0]
-print deg
+	print float(arousal)
+	print float(valence)
 
-# compute value with smallest angular distance
+	deg=np.array(map(lambda x:360+x if x<0 else x,np.arctan2(arousal,valence)*360/(2*np.pi)))[0]
+	#print deg
 
-difs=abs(np.arange(0,360,45)-float(deg))
-best=difs.argmin()
-if(360.-float(deg)<difs.min()):
-    best=0
+	# compute value with smallest angular distance
 
-print difs
+	difs=abs(np.arange(0,360,45)-float(deg))
+	best=difs.argmin()
+	if(360.-float(deg)<difs.min()):
+	    best=0
 
-print emotions[best]
+	#print difs
 
-#print np.array(emotions.keys())-deg)
+	print emotions[best]
+
+	#print np.array(emotions.keys())-deg)
 
 
-[["Word","V.Mean.Sum"]]
