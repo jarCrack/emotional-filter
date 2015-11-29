967dcebb06c738545354a1844178aa6323c48556
jmalich <malich@uni-potsdam.de>
2015-11-29 02:01:18 +0100

    excpetion handling, if word not found

--- analysis/detect_emotion.py
+++ analysis/detect_emotion.py
@@ -8,43 +8,46 @@ import sys
 if(len(sys.argv)<2):
 	print "Please input tag"
 else:
-	#print sys.argv[1]
+	try:
+		#print sys.argv[1]
 
-	word=sys.argv[1]
-	emotions=["pleased","excited","aroused","distressed","miserable","depressed","sleepy","relaxed"]
+		word=sys.argv[1]
+		emotions=["pleased","excited","aroused","distressed","miserable","depressed","sleepy","relaxed"]
 
-	"aroused,distressed,miserable,depressed,sleepy,relaxed,pleased,excited"
+		"aroused,distressed,miserable,depressed,sleepy,relaxed,pleased,excited"
 
-	#scan csv
+		#scan csv
 
-	df=pandas.read_csv("ratings.csv")
+		df=pandas.read_csv("ratings.csv")
 
-	#print df
-	entry=df[df["Word"]==word]
-	valence=entry["V.Mean.Sum"]
-	arousal=entry["A.Mean.Sum"]
+		#print df
+		entry=df[df["Word"]==word]
+		valence=entry["V.Mean.Sum"]
+		arousal=entry["A.Mean.Sum"]
 
-	# compute degree
-	arousal-=5.
-	valence-=5.
+		# compute degree
+		arousal-=5.
+		valence-=5.
 
-	print float(arousal)
-	print float(valence)
+		print float(arousal)
+		print float(valence)
 
-	deg=np.array(map(lambda x:360+x if x<0 else x,np.arctan2(arousal,valence)*360/(2*np.pi)))[0]
-	#print deg
+		deg=np.array(map(lambda x:360+x if x<0 else x,np.arctan2(arousal,valence)*360/(2*np.pi)))[0]
+		#print deg
 
-	# compute value with smallest angular distance
+		# compute value with smallest angular distance
 
-	difs=abs(np.arange(0,360,45)-float(deg))
-	best=difs.argmin()
-	if(360.-float(deg)<difs.min()):
-	    best=0
+		difs=abs(np.arange(0,360,45)-float(deg))
+		best=difs.argmin()
+		if(360.-float(deg)<difs.min()):
+		    best=0
 
-	#print difs
+		#print difs
 
-	print emotions[best]
+		print emotions[best]
 
-	#print np.array(emotions.keys())-deg)
+		#print np.array(emotions.keys())-deg)
+	except TypeError:
+		print 0
 
 
